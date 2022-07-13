f67e66d985a86f818f804f0804e3b4b59a4b9757
Irina <ideryusheva@inbox.ru>
2022-07-13 23:09:56 +0700

    HomeWork2

--- HomeWork2/HomeWork2.5.py
+++ HomeWork2/HomeWork2.5.py
@@ -1,2 +1,11 @@
 # Реализуйте алгоритм перемешивания списка.
 
+def listSort(n):
+    for i in range(len(n) // 2):
+        helpVar = n[i]
+        n[i] = n[-i - 1]
+        n[-i - 1] = helpVar
+
+n = ['привет', 'дорогой', 'друг', 'я', 'рад', 'тебя', 'видеть']
+listSort(n)
+print(n)
